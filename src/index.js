0031a140811f8a86510fca1de4974adb08173d81
alena-vovkohon <alena.vovkogon@gmail.com>
2022-05-03 14:42:51 +0300

    home work #8

--- src/index.js
+++ src/index.js
@@ -4,107 +4,57 @@ import { goods } from './goods.js';
  * @param {string} brand
  * @returns {*}
  */
-const brandFilter = (brand) => {
-  const newArray = goods.filter((item) => item.brand === brand)
-  return newArray
-};
+const brandFilter = (brand) => goods.filter((item) => item.brand === brand);
 
 /**
  * @param {string} color
  * @returns {*}
  */
-const colorFilter = (color) => {
-  const newArray = goods.filter((item) => item.color === color)
-  return newArray
-};
+const colorFilter = (color) => goods.filter((item) => item.color === color);
 
 /**
  * @param {string} model
  * @returns {*}
  */
-const modelFilter = (model) => {
-  const newArray = goods.filter((item) => item.model === model)
-  return newArray
-};
+const modelFilter = (model) => goods.filter((item) => item.model === model);
 
 /**
  * @param {number} memory
  * @returns {*}
  */
-const memoryFilter = (memory) => {
-  // console.log(Number(memory))
-  const newArray = goods.filter((item) => item.memory === Number(memory))
-  return newArray
-};
+const memoryFilter = (memory) => goods.filter((item) => item.memory === Number(memory));
 
 /**
  * @param {number} price
  * @returns {*}
  */
-const priceFilter = (price) => {
-  const newArray = goods.filter((item) => item.price === Number(price))
-  return newArray
-};
+const priceFilter = (price) => goods.filter((item) => item.price === Number(price));
 
 /**
  * @param {string} country
  * @returns {*}
  */
-const countryFilter = (country) => {
-  const newArray = goods.filter((item) => item.country === country)
-  return newArray
-};
+const countryFilter = (country) => goods.filter((item) => item.country === country);
 
 /**
  * @param {string} os
  * @returns {*}
  */
-const osFilter = (os) => {
-  const newArray = goods.filter((item) => item.os === os)
-  return newArray
-};
+const osFilter = (os) => goods.filter((item) => item.os === os);
 
 /**
  * @param {number} from
  * @param {number} to
  */
-const rangeFilter = (from, to) => {
-  const newArray = goods.filter((item) => item.price <= Number(to) && item.price >= Number(from))
-  return newArray
-};
+const rangeFilter = (from, to) => goods.filter((item) => item.price <= Number(to) && item.price >= Number(from));
 
-const minPriceReducer = () => {
-  const minPrice = goods.reduce((accumulator, current) => {
-    return Math.min(accumulator, current.price)
-  }, 12999);
-  return minPrice
-};
+const minPriceReducer = () => goods.reduce((accumulator, current) =>  Math.min(accumulator, current.price), Infinity);
 
+const maxPriceReducer = () => goods.reduce((accumulator, current) => Math.max(accumulator, current.price), -Infinity);
 
-const maxPriceReducer = () => {
-  const maxPrice = goods.reduce((accumulator, current) => {
-    return Math.max(accumulator, current.price) 
-  }, 0);
-  return maxPrice
-};
+const toMaxSorter = () => goods.sort((a, b) => b.price - a.price);
 
-const toMaxSorter = () => {
-  const newArray = goods.sort((a, b) => {
-    if (a.price < b.price) return 1
-    if (a.price > b.price) return -1
-    return 0
-  })
-  return newArray
-};
-
-const toMinSorter = () => {
-  const newArray = goods.sort((a, b) => {
-    if (a.price > b.price) return 1
-    if (a.price < b.price) return -1
-    return 0
-  })
-  return newArray
-};
+const toMinSorter = () => goods.sort((a, b) => a.price - b.price);
 
 export const filters = {
   brandFilter,
